cfeb9d089e327cebebaf31789d81762e15859f72
Matthew Wilson <automationator@runbox.com>
2018-08-30 14:33:03 -0400

    Fixes README.md inclusion

--- setup.py
+++ setup.py
@@ -23,10 +23,11 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.4',
+    version='0.1.5',
 
     description='Library that interacts with CRITS to build an indicator whitelist system.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/automationator/critswhitelist',
